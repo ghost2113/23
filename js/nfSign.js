324b384f68421a8a550881771c0d87c9295c5e6e
tianjinying <tianjinying93@163.com>
2018-04-04 20:50:12 +0800

    44

--- js/nfSign.js
+++ js/nfSign.js
@@ -8,10 +8,11 @@
 		    return null; 
 		};		
 		var userID = getUrlParams("userId");
-		userID = 3;
+		userID = 11;
 		console.log({"用户userId":userID,"地址":window.location.search});
 		var olDate;//后台返回线上日期
-        var dateArray = []; //遍历网格		        
+        var dateArray = []; //遍历网格	
+        var dateM = [];
         var dateList = [];//定义空数组,存储已经签到日期，向后台传送
         var $dateBox = $("#js-qiandao-list"),
             $currentDate = $(".current-date"),
@@ -32,15 +33,12 @@
         
         
         //是否请求到用户信息，模拟日历 false
-        if(!userID){
-        	console.log(111111111111111111);
-        	_handle = false;
-        	for (var i = 0; i < totalDay; i++) {
-	        	//设置日历 日期起始位置
-	            $dateLi.eq(i + monthFirst).addClass("date" + parseInt(i + 1));		            
-	            $dateLi.eq(i + monthFirst).text(i+1);        
-	        } 
-        }
+        if(userID==null){_handle=false;}//如果没有用户userID禁止签到
+    	for (var i = 0; i < totalDay; i++) {
+        	//设置日历 日期起始位置
+            $dateLi.eq(i + monthFirst).addClass("date" + parseInt(i + 1));		            
+            $dateLi.eq(i + monthFirst).text(i+1);        
+       } 
         //请求用户已经签到信息，存储到数组中去
         axios.post("http://47.96.1.236:8080/zaotoutiao-api-home-1.0.0/sign/in?userId="+userID+"&isSubmit=0").then(function(response){
         	console.log(response.data);
@@ -51,47 +49,53 @@
     		var signList = new Array();
     		var _index;
 			//判断是否有签到日期
-    		if(response.data.signList){
-	    		response.data.signList.forEach(function(items,index){
-	    			if(items.signContinuousDay==1){
-	    				signList.unshift(items);
-	    			};
-	    			if(items.createDate == signList[signList.length-1].createDate){
-	    				_index = index;
-	    			};    			
-	    			if(index<(_index+1)){
-	    				dateArray.unshift(items.createDate.split(" ")[0].substr(8,2));
-	    			};    			
-	    		});
-    		}else{
-    			dateArray = [];
-    		}    		
+			console.log("已经签到数组",response.data.signList);			
+
+			if(!response.data.signList){
+				console.log("kong");
+				dateArray=[];
+			}else{	
+				response.data.signList.forEach(function(items,index){    			
+    				dateArray.push(items.createDate.split(" ")[0].substr(8,2));
+    			});    			
+				/*dateArray.unshift(response.data.signList[i].createDate.split(" ")[0].substr(8,2));*/
+				console.log("签到渲染数组1",dateArray);
+			}	
 	    	$dateLi.each(function(index,item){	    	
 		    	if((index+1-monthFirst) < myDate.getDate()){
 					item.style.color = "#b2b2b2";						
 		    	}			
 			});
 	    	console.log("签到渲染数组",dateArray);
-	        for (var i = 0; i < totalDay; i++) {
+/*		    dateArray.forEach(function(items,index){
+	        	if(dateArray[index]==parseInt(dateArray[index+1])+1){
+	        		dateM.unshift(items);
+	        	}
+	        });*/
+	       for(var k=0;k<dateArray.length;k++){
+       			if(dateArray[k]==parseInt(dateArray[k+1])+1){
+	        		dateM.unshift(dateArray[k]);
+	        		console.log(dateArray[k]);
+        		}	       		
+	       }
+	       dateM.unshift(dateArray[dateM.length]);
+	        console.log("dateM",dateM);
+	        for (var i = 0; i < totalDay; i++) {	        	
 	        	//设置日历 日期起始位置
 	            $dateLi.eq(i + monthFirst).addClass("date" + parseInt(i + 1));		            
 	            $dateLi.eq(i + monthFirst).text(i+1);
 	            //当前日期之前字体改变
-	            //显示已经签到的日期
-	            /*console.log(dateArray);*/	           	     	       
-	            for (var j = 0; j < dateArray.length; j++) {
-	            	console.log("ll");
-	                if (i == dateArray[j]) {	
-	                	console.log("ok");
+	            //显示已经签到的日期	
+	            for (var j = 0; j < dateM.length; j++) {	            	
+	                if (i == dateM[j]) {		                	
 	                    $dateLi.eq(i + monthFirst-1).addClass("qiandaoActive");
 	                    //签到成功之后，按签到天数显示奖励			 
 	                }
 	            }
 	        } 
 	        //渲染持续签到奖励
-           	[parseInt(dateArray[0])+7,parseInt(dateArray[0])+14,parseInt(dateArray[0])+21,parseInt(dateArray[0])+28].forEach(function(item,index){
+           	[parseInt(dateM[0])+7,parseInt(dateM[0])+14,parseInt(dateM[0])+21,parseInt(dateM[0])+28].forEach(function(item,index){
         		$(".date" + item).addClass('qiandaoActive'+index);
-	        		/*$(".date" + item).text(_qiandaoJl[index]);*/
 	        })
     	}).catch(function(error){
     		console.log(error);
@@ -134,38 +138,45 @@
         	//判断签到日期前一天是否未签
     	    if($(".date" + myDate.getDate()-1).hasClass("qiandaoActive")){
     			$(".date" + myDate.getDate()).addClass('qiandaoActive');
-    			console.log("ok");
-    			//签到成功之后，按签到天数显示奖励
-	        	/*[myDate.getDate()+7,myDate.getDate()+14,myDate.getDate()+21,myDate.getDate()+28].forEach(function(item,index){
-	        		$(".date" + item).addClass('qiandaoActive'+index);
-	        	})*/
         	}else{
         		$(".date" + myDate.getDate()).addClass('qiandaoActive').siblings().removeClass('qiandaoActive');
-        		//如果未签，则清空数组
-    			dateList=[];
+        		
         	}
-
+			
 	    	//向后台发送签到成功请求 把签到信息追加到signList数组中去
 	    	axios.post("http://47.96.1.236:8080/zaotoutiao-api-home-1.0.0/sign/in?userId="+userID+"&isSubmit=1")
 	    	.then(function(response){
-	    		if(response.data.msg=="今天已经签到"){
-	    			wake();
+	    		console.log(response);
+	    		if(response.data.msg=="未连续签到，从头开始签到"){
+	    			console.log("未连续签到，从头开始签到");
+	    			$(".qiandaoActive0").removeClass("qiandaoActive0");
+	    			$(".qiandaoActive1").removeClass("qiandaoActive1");
+	    			$(".qiandaoActive2").removeClass("qiandaoActive2");
+	    			$(".qiandaoActive3").removeClass("qiandaoActive3");
+	    			//渲染持续签到奖励
+		           	[myDate.getDate()+7,myDate.getDate()+14,myDate.getDate()+21,myDate.getDate()+28].forEach(function(item,index){
+		        		$(".date" + item).addClass('qiandaoActive'+index);
+			        		/*$(".date" + item).text(_qiandaoJl[index]);*/
+			        })
 	    		}
-	    		console.log(response);	    		
+	    	var getGold = response.data.getGold;
+	    		console.log(getGold);
+	    		signIn(getGold);	
+	    		
 	    	}).catch(function(error){
 	    		console.log(error);
 	    	})
         }
-        function wake(){    
+        function signIn(getGold){    
 	       var u = navigator.userAgent;
 	       var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
 	       var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 	       if(isAndroid){
 	            alert('是否是Android：'+isAndroid);
-	           window.android.wake("ok")
+	           window.android.signIn(getGold)
 	       }else if(isiOS){
 	            alert('是否是iOS：'+isiOS);
-	           window.webkit.messageHandlers.wake.postMessage("ok");
+	           window.webkit.messageHandlers.signIn.postMessage(getGold);
 	       }
 		}        
     }();
